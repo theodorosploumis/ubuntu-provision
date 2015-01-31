f158df18a32d1e77fdd9f80d2bd665794396d81e
Theodoros Ploumis <me@theodorosploumis.com>
2015-02-01 01:05:23 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -18,12 +18,8 @@ cd ubuntu-provision
 
 sudo pip install -r requirements.txt
 
-echo -e "Enter your username and press [ENTER]" && read PROVISIONING_USER
 sudo sed -i "s/^username: .*/username: $PROVISIONING_USER/" roles/common/vars/main.yml
-
-echo -e "Enter your github username and press [ENTER]" && read PROVISIONING_GITHUB_USERNAME
 sudo sed -i "s/^github_username: .*/github_username: $PROVISIONING_GITHUB_USERNAME/" roles/common/vars/main.yml
-
 sudo sed -i "s/^uname_r: .*/uname_r: `uname -r`/" roles/common/vars/main.yml
 
 ansible-playbook setup.yml -i HOSTS --ask-sudo-pass  --module-path ./ansible_modules
