3cce59769f4171bbe45dbe630603b67c77fcc7f7
Theodoros Ploumis <me@theodorosploumis.com>
2015-02-03 02:11:30 +0200

    Update run_ansible.sh

--- run_ansible.sh
+++ run_ansible.sh
@@ -1,2 +1,10 @@
-ansible-playbook setup.yml -i HOSTS --module-path ./ansible_modules
+cd ~
+mkdir -p setup
+cd setup
+git clone https://github.com/TheodorosPloumis/ubuntu-provision.git
+cd ubuntu-provision
+
+sudo pip install -r requirements.txt
+
+ansible-playbook setup.yml -i HOSTS --ask-sudo-pass --module-path ./ansible_modules
 exit 0
