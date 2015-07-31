06da8db4cab1ce8836189757cce46040c6a2aaa7
Theodoros Ploumis <me@theodorosploumis.com>
2015-07-31 19:42:21 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,14 +1,22 @@
 # Install required software
 sudo apt-get install python-setuptools aptitude git curl python-dev libxml2-dev libxslt-dev
 
+# Install Python pip
 sudo easy_install pip
 
+# Clone the repository
 cd ~
 git clone https://github.com/TheodorosPloumis/ubuntu-provision.git ~/ubuntu-provision
 cd ~/ubuntu-provision
 
+# Change permissions and owner of the folder
+sudo chown $USER:$USER ~/ubuntu-provision
+sudo chmod -R 777 ~/ubuntu-provision
+
+# Install Ansible requirements
 sudo pip install -r requirements.txt
 
-ansible-playbook setup.yml -i HOSTS --ask-sudo-pass  --module-path ./ansible_modules
+# Run Ansible...
+ansible-playbook main.yml -i HOSTS --ask-sudo-pass  --module-path ./ansible_modules
 
 exit 0
